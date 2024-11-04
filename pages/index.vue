0b84fada97caab47cdd75c6a01e9f0d73c78db91
Han Wang <hanforwork896@gmail.com>
2024-11-04 23:33:40 +0800

    fix: remove  syntax

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+</script>
 
 <template>
   <div>
     <h1>Page: index 首頁</h1>
-    <div>當前路由: {{ $route.fullPath }}</div>
+    <div>當前路由: {{ route.fullPath }}</div>
   </div>
 </template>
 
